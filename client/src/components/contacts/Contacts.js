e001505323bd58016d1c3c6d70ffc01e8fe7f32d
PGOKULNATH <4gokul2000@gmail.com>
2019-12-19 18:30:28 +0530

    contact front-end finished

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,35 @@
+import React, {useContext, Fragment} from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import ContactItem from './ContactItem';
+import ContactContext from '../../context/contact/ContactContext';
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+  const {contacts, filtered} = contactContext;
+
+  if(contacts.length === 0){
+    return <h4>Please add a contact</h4>
+  }
+
+  return(
+    <Fragment>
+      <TransitionGroup>
+        {
+          filtered !== null ?
+          filtered.map(contact => (
+            <CSSTransition timeout = {500} key={contact.id} classNames="item">
+              <ContactItem contact={contact}/>
+            </CSSTransition>
+          )) :
+          contacts.map(contact => (
+            <CSSTransition timeout = {500} key={contact.id} classNames="item">
+              <ContactItem contact={contact}/>
+            </CSSTransition>
+          ))
+        }
+      </TransitionGroup>
+    </Fragment>
+  );
+}
+
+export default Contacts;
